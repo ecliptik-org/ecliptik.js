c216260ee58364cb0ffdf66d901e7b31a79ecc29
Philipp Reitter <p.reitter@gmail.com>
2018-04-14 11:17:14 +0200

    test commit please ignore

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var concat = require('gulp-concat');
+var rm = require("gulp-rimraf");
+
+gulp.task('clean', function() {
+	    gulp.src('dist/*').pipe(rm());
+});
+
+gulp.task('bundle', function() {
+	return gulp.src('./src/*.js')
+	    .pipe(concat("ecliptik.js"))
+	    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify', function() {
+	return gulp.src('./dist/ecliptik.js')
+	    .pipe(minify({
+		    ext: {
+			    src:".js",
+			    min:".min.js"
+		    }
+	    }))
+	    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task("build", ["clean", "bundle", "minify"]);
+gulp.task("default", ["build"]);
