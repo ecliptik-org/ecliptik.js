dc23a2b0c1f2d3b3b2de9cd93e420963f30cf067
Philipp Reitter <p.reitter@gmail.com>
2018-04-14 11:53:48 +0200

    test commit please ignore

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,29 @@ var minify = require('gulp-minify');
 var concat = require('gulp-concat');
 var rm = require("gulp-rimraf");
 
-gulp.task('clean', function() {
-	    gulp.src('dist/*').pipe(rm());
+gulp.task('clean', function () {
+    gulp.src('dist/*').pipe(rm());
 });
 
-gulp.task('bundle', function() {
-	return gulp.src('./lib/*.js')
-	    .pipe(concat("ecliptik.js"))
-	    .pipe(gulp.dest('./dist/'));
+gulp.task('bundle', function () {
+    return gulp.src([
+        './lib/jquery-ext.js',
+        './lib/params.js',
+        './lib/ecliptik.js'
+    ])
+        .pipe(concat("ecliptik.js"))
+        .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('minify', function() {
-	return gulp.src('./dist/ecliptik.js')
-	    .pipe(minify({
-		    ext: {
-			    src:".js",
-			    min:".min.js"
-		    }
-	    }))
-	    .pipe(gulp.dest('./dist/'));
+gulp.task('minify', function () {
+    return gulp.src('./dist/ecliptik.js')
+        .pipe(minify({
+            ext: {
+                src: ".js",
+                min: ".min.js"
+            }
+        }))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task("build", ["clean", "bundle", "minify"]);
